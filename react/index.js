e5fcd8dd75dc277d1685030735f31b2a46fba6e0
Shashank R <rahatekars@gmail.com>
2025-02-26 19:37:05 +0530

    React Introduced(keeping local files)

---
+++ react/index.js
@@ -0,0 +1,9 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+console.log(document.getElementById('root'));
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+
+// ReactDOM.render(<App />, );
