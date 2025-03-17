7a9b0e00d80889434f9243adaacfbf75b7a0a700
Shashank R <rahatekars@gmail.com>
2025-03-17 13:48:06 +0530

    Catalog mannual sync

--- src/components/common-components/Card.js
+++ src/components/common-components/Card.js
@@ -2,12 +2,14 @@ import React, { Fragment } from "react";
 import { CardMedia, CardContent, Typography } from '@mui/material';
 import { Link } from "react-router-dom";
 
+const BASE_URL = 'https://mmg.whatsapp.net/v/t45.5328-4';
+
 export default ({ product }) => {
     return (
         <Fragment>
             {/* <Link to={`/shop/${product.id}`}> */}
             <Link to={`https://wa.me/p/${product.id}/917047626500`} target="_blank">
-                <CardMedia sx={{ height: "auto" }} component="img" height="150" image={product.media.images[0].original_image_url} alt={product.name} />
+                <CardMedia sx={{ height: "auto" }} component="img" height="150" image={BASE_URL + product.media.images[0].original_image_url} alt={product.name} />
             </Link>
             <CardContent sx={{ display: "flex", flexDirection: "column", flexGrow: 1, justifyContent: "space-between", textAlign: 'left' }}>
                 <Typography variant="body1">{product.name}</Typography>
