c7002737f511a24bc75c48c8bd1f78e27edaf42d
Ashish Wadbude <117153575+ashish21356@users.noreply.github.com>
2025-03-20 12:16:07 +0530

    Update Body.js

--- src/components/Body.js
+++ src/components/Body.js
@@ -9,7 +9,9 @@ const Body = () => {
        
 
         <Grid container spacing={2} justifyContent={{ md: "center" }} sx={{ padding: { xs: 1, sm: 3 } }}>
-            <h1 class="text-4xl font-bold text-center text-blue-600">Our New Products</h1>
+           <Grid item xs={12}>
+        <h1 className="text-4xl font-bold text-center text-blue-600">Our New Products</h1>
+           </Grid>
             {products.map((product) => (
                 <Grid item key={product.id} xs={6} sm={6} md={2}>
                     <Card className="body-card" sx={{ height: "100%", textAlign: "center", display: "flex", flexDirection: "column", boxShadow: 3, minWidth: { xs: 180, sm: 250, md: 220 }, maxWidth: { xs: 180, sm: 250, md: 300 }, margin: "auto" }}>
